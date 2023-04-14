0bba0e43557aa139bca0edbcc53a6205792c24da
Dagim <dagimcodes@gmail.com>
2023-04-14 21:25:51 +0300

    Splash Screen done

--- app/src/main/java/tg/dagno2/enebla/User.kt
+++
@@ -1,9 +0,0 @@
-package tg.dagno2.enebla
-
-data class User(
-    val firstName: String = "Ablex",
-    val lastName: String = "Birhanu",
-    val email: String = "email@gmail.com",
-    val gender: Boolean = true,
-    val phoneNumber: String = "+251"
-    )
--- app/src/main/java/tg/dagno2/enebla/login_signup/LogIn.kt
+++ app/src/main/java/tg/dagno2/enebla/login_signup/LogIn.kt
@@ -1,25 +1,48 @@
 package tg.dagno2.enebla.login_signup
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.isVisible
+import kotlinx.coroutines.*
 //import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
 import tg.dagno2.enebla.HomePage
 import tg.dagno2.enebla.databinding.ActivityLogInBinding
+import tg.dagno2.enebla.firstTimer.Boarding
 
 class LogIn : AppCompatActivity() {
 
 //    lateinit var auth: FirebaseAuth // create instance of firebase auth
     private lateinit var binding: ActivityLogInBinding
+    private var isLoading = true
+    private suspend fun junction() {
+        val sharedPreferences = getSharedPreferences("LoadUp", Context.MODE_PRIVATE)
+        if (sharedPreferences.getBoolean("first_time",true)|| true){
+            val onBoarding = Intent(this, Boarding::class.java)
+            onBoarding.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(onBoarding)
+        }
+//        if (auth.currentUser != null) {
+//            val homePage = Intent(this, HomePage::class.java)
+//            login.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//            startActivity(homePage)
+//        }
+        delay(500L)
+        isLoading = false
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                isLoading
+            }
+        }
+        runBlocking {
+            junction()
+        }
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 //        auth = FirebaseAuth.getInstance()
